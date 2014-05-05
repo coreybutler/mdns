f492ca19525acf3f60d3497e96d0f46d64f9512d
coreybutler <=corey@coreybutler.cmom>
2014-05-05 16:10:40 -0500

    Initial code for JMDNS

--- www/MDNS.js
+++ www/MDNS.js
@@ -1,15 +1,46 @@
-var cordova = require('cordova');
+/**
+ * MDNS plugin for Cordova/Phonegap
+ *
+ * Modified from:
+ * Copyright (c) 2013 Vlad Stirbu <vlad.stirbu@ieee.org> Converted to Cordova 3.0 format
+ * MIT license
+ *
+ * @author Matt Kane
+ * Copyright (c) Triggertrap Ltd. 2012. All Rights Reserved.
+ * Available under the terms of the MIT License.
+ *
+ */
+
+/*global module, console, require*/
+/*jshint -W097 */
+'use strict';
+var exec = require('cordova/exec');
 
 var MDNS = {
-  test: function(callback){
-    alert('Running cordova exec');
-    cordova.exec(
-        callback, // success callback function
-        callback, // error callback function
-        'MDNS', // mapped to our native Java class called "Calendar"
-        "testAction",
-        ["firstArgument", "secondArgument", 42, false]
-    );
+  watch: function (type, callback) {
+    return exec(function (result) {
+      callback && callback(result);
+    }, MDNS.fail, "MDNS", "watch", [type]);
+  },
+  unwatch: function (type) {
+    return exec(null, MDNS.fail, "MDNS", "unwatch", [type]);
+  },
+  close: function () {
+    return exec(null, MDNS.fail, "MDNS", "close", []);
+  },
+  register: function (type, name, port, text) {
+    if (!type) {
+      console.error("'type' is a required field");
+      return;
+    }
+    return exec(null, MDNS.fail, "MDNS", "register", [type, name, port, text]);
+  },
+  unregister: function () {
+    return exec(null, MDNS.fail, "MDNS", "unregister", []);
+  },
+  fail: function (o) {
+    console.error("Error " + JSON.stringify(o));
   }
-}
+};
+
 module.exports = MDNS;
