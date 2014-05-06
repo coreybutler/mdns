73715d501c933f3783f55e098e93a8a3aec2c1fa
coreybutler <=corey@coreybutler.cmom>
2014-05-06 16:10:40 -0500

    Tweaks

--- www/MDNS.js
+++ www/MDNS.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-var MDNS = function(type){
+var MDNS = function(_type){
 
   Object.defineProperties(this,{
     handlers:{
@@ -9,12 +9,12 @@ var MDNS = function(type){
       configurable: true,
       value: {}
     },
-//    oncehandlers:{
-//      enumerable: false,
-//      writable: true,
-//      configurable: true,
-//      value: {}
-//    },
+    oncehandlers:{
+      enumerable: false,
+      writable: true,
+      configurable: true,
+      value: {}
+    },
     on: {
       enumerable: true,
       writable: false,
@@ -24,28 +24,27 @@ var MDNS = function(type){
         this.handlers[eventName].push(fn);
       }
     },
-//    once: {
-//      enumerable: true,
-//      writable: false,
-//      configurable: false,
-//      value: function(eventName,fn){
-//        this.oncehandlers[eventName] this.oncehandlers[eventName] || [];
-//        this.oncehandlers[eventName].push(fn);
-//      }
-//    },
+    once: {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: function(eventName,fn){
+        this.oncehandlers[eventName] = this.oncehandlers[eventName] || [];
+        this.oncehandlers[eventName].push(fn);
+      }
+    },
     handleEvent: {
       enumerable: false,
       writable: false,
       configurable: false,
       value: function(data){
-//        (this.handlers[data.action]||[]).forEach(function(handler){
-//          handler.apply(me,[data]);
-//        });
-//        (this.oncehandlers[data.action]||[]).forEach(function(handler){
-//          handler.apply(me,[data]);
-//        });
-//        this.oncehandlers.hasOwnProperty(data.action) && delete this.oncehandlers[data.action];
-        alert('handleEvent');
+        (this.handlers[data.action]||[]).forEach(function(handler){
+          handler.apply(me,[data]);
+        });
+        (this.oncehandlers[data.action]||[]).forEach(function(handler){
+          handler.apply(me,[data]);
+        });
+        this.oncehandlers.hasOwnProperty(data.action) && delete this.oncehandlers[data.action];
       }
     },
     listen: {
@@ -53,17 +52,19 @@ var MDNS = function(type){
       writable: false,
       configurable: false,
       value: function(type){
+        type = type || null;
         var me = this;
 
         return exec(function(result) {
           var data = typeof result === 'object' ? result : {
             action: 'unknown',
           };
-          if (type){
+          if (type !== null){
             data.action === type && me.handleEvent(data);
             return;
+          } else {
+            me.handleEvent(data);
           }
-          me.handleEvent(data);
         }, function(e){
           throw e;
         }, "MDNS", "monitor", []);
@@ -71,40 +72,7 @@ var MDNS = function(type){
     }
   });
 
-  this.listen(type);
+  this.listen(_type);
 };
 
 module.exports = MDNS;
-
-//  Object.defineProperty(this,{
-//    events: {
-//      enumerable: false,
-//      writable: true,
-//      configurable: false,
-//      value: {}
-//    },
-//    listen: {
-//      enumerable: true,
-//      get: function(){
-//        var me = this;
-//        return exec(function(result) {
-//          alert('BLUNT FORCE TEST: '+result);
-//          me.emit('evt',result);
-//        }, this.fail, "MDNS", "monitor", [type]);
-//      }
-//    },
-//    fail: {
-//      enumerable: false,
-//      writable: false,
-//      configurable: false,
-//      value: function(e){
-//        throw new Error(e);
-//      }
-//    },
-//    on: function(evt,fn){
-//      this.events[evt] = this.events[evt] || [];
-//      this.events[evt]
-//    }
-//  });
-//
-//  this.listen();
